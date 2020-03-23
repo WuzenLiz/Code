158d1f930fc1e2ec30c66ff9fbff056819284faa
macgia99 <macgia1999@gmail.com>
2020-03-24 05:08:18 +0700

    Added PIE_Problem_2.py

---
+++ ThucHanh/PIE_Problem_2.py
@@ -0,0 +1,43 @@
+import math
+import time as tm
+#define 
+PI=math.acos(-1.0)
+n=0
+f=0
+a=[]
+va=[]
+
+def check(md):
+    sum=0
+    for i in va:
+        sum+=int(i/md)
+    return sum
+    
+def main():      
+    ans,l,v=0.0,0.0,0.0
+    n,f=input("Nhập số miếng bánh và số khách: ").split()
+    f=int(f)+1
+
+    for i in range(int(n)):
+        a.append(int(input("Nhập bán kính của bánh thứ %d: " %(i+1))))
+    for i in a:
+        v=pow(i,2)*PI
+        va.append(v)
+
+    s=50
+    r=1000000.0
+    while (s>=0):
+        sum=0
+        mid=(r+l)/2
+        if(check(mid)>=f):
+            ans=mid
+            l=mid
+        else:
+            r=mid
+        s-=1
+    print('%.6f' % ans)
+
+if __name__ == '__main__':
+    start = tm.time()
+    main()
+    print('Đã tốn %s giây để thực hiện' % (tm.time() - start))
